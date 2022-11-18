ef21c41cf6858a9c2acee4b5ef24e5c6a34f5759
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2022-11-18 14:39:17 +0100

    typo

--- src/main/js/components/Introduction.js
+++ src/main/js/components/Introduction.js
@@ -10,7 +10,7 @@ export default class Introduction extends React.Component {
                 <Row>
                     <p style={{textAlign: "justify"}}>Natural Products Online is an open source project for Natural Products (NPs) storage, search and analysis.
                         This page hosts LOTUS, one of the biggest and best annotated resources for NPs occurrences available free of charge and without any restriction.
-                        LOTUS is a living database, which is hosted both here and on <a  target="_blank" rel="noopener noreferrer" href={"https://www.wikidata.org/"}> Wikidata</a>.
+                        LOTUS is a living database, which is hosted both here and on <a  target="_blank" rel="noopener noreferrer" href={"https://www.wikidata.org/"}>Wikidata</a>.
                         The Wikidata version allows for community curation and addition of novel data. 
                         The current version allows a more user friendly experience (such as structural search, taxonomy oriented query, flat table and structures exports).
                         <br/>
